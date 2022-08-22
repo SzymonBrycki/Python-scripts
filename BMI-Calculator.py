de5966c04b1a15c2234938d7e62f391f66b96b07
Szymon Brycki <szymon.brycki@gmail.com>
2022-08-22 16:33:03 +0200

    Add files via upload

---
+++ BMI-Calculator.py
@@ -0,0 +1,8 @@
+print("Wlecome to the BMI Calculator. Enter your data balow.")
+
+mass = int(input("Your weight in kilograms? "))
+height = float(input("Your height in meters? "))
+
+BMI = mass / height**2
+
+print("Your BMI is %.2f" % BMI)
