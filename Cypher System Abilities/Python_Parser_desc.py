126dc5dbe8a751fe02cea34377e07021fd6ea03f
Szymon Brycki <szymon.brycki@gmail.com>
2024-03-09 02:36:14 +0100

    Add files via upload

---
+++ Cypher System Abilities/Python_Parser_desc.py
@@ -0,0 +1,61 @@
+ENG_list = list()
+ENG_list_2 = list()
+POL_list = list()
+
+beginning = "("
+ending = ")" 
+
+sampleString = "English (Polski)"
+
+def findingPOLbeginning(string):
+    index = string.rfind(beginning)
+    return index
+
+def findingPOLending(string):
+    index = string.rfind(ending)
+    return index
+
+def findingPOLword(string):
+    beginning = findingPOLbeginning(string) + 1 # +1 to exclude the "("
+    ending = findingPOLending(string)
+
+    POLcontent = string[beginning:ending]
+    return POLcontent
+
+def findingENGword(string):
+    ending = findingPOLbeginning(string) #Polish beginning is an English ending
+
+    ENcontent = string[:ending - 1]
+    return ENcontent
+
+with open("Słownik - deskryptory (ANG to POL).txt", encoding="utf8") as read_file:
+    ENG_list = read_file.readlines()
+
+# print(ENG_list) # OK!!!
+
+### ENG_list_2 = [item[6:] for item in ENG_list]
+
+# print(ENG_list_2)
+
+# print(findingPOLbeginning(sampleString))
+# print(findingPOLending(sampleString))
+    
+# print("PL word: ", findingPOLword(sampleString))
+# print("EN word: ", findingENGword(sampleString))
+
+for item in ENG_list:
+    PL_word = findingPOLword(item)
+    EN_word = findingENGword(item)
+
+    return_string = PL_word + " (" + EN_word + ")\n"
+
+    POL_list.append(return_string)
+
+POL_list.sort()
+
+print(POL_list)
+
+with open("Słownik - deskryptory (POL to ANG).txt", "w", encoding="utf8") as save_file:
+    save_file.writelines(POL_list)
+
+
